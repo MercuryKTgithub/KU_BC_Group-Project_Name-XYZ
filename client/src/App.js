b316f5bfd23d51e6b2f1153a22ad6f7941d5f9ca
MercuryKTgithub <KimDotnetPossible@att.net>
2022-05-26 03:58:05 -0500

    Phase I: Adding some basic component folder of the homepage, delete or override old default files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
+
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch'; 
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({uri: '/graphql',});
+
+// -- Use the setContext() function to retrieve the token from localStorage 
+// and set the HTTP request headers of every request to include the token
+// -- Because we're not using the first parameter, but we still need to access 
+// the second one, we can use an underscore _ to serve as a placeholder for the first parameter.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token_in_deepthotapp');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// -- Create and instruct the Apollo instance to retrieve this token every time we make a GraphQL request.
+// by combining the authLink and httpLink objects so that every request can retrieve the token and sets 
+// the request headers before making the request to the API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route   
+                path="/" 
+                element={<Home />} 
+              />                             
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
