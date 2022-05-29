2d695d3b04522874da9053eb79636196a42aee6e
bryce <blarkin13@gmail.com>
2022-05-29 01:32:19 -0500

    photo array

--- client/src/components/PhotoList/index.js
+++ client/src/components/PhotoList/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import photo from '../../assets/small/wk-1.jpg'
 
-function PhotoList() {
-    return(
+const PhotoList = ({ category }) => {
+
+    const [photos] = useState([
+        
+        {
+            name: "elegent cake",
+            category: "wedding",
+            description: "user ordered cake",
+        },
+        {
+            name: "fancy cake",
+            category: "wedding",
+            description: "user ordered cake",
+        },
+      ]);
+
+      const currentPhotos = photos
+
+    return (
         <div>
-            <img
-            src={photo}
-            alt="wedding cake"
-            />
-
+            <div className='flex-row'>
+                {currentPhotos.map((image, i) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${i}.jpg`)}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        key={image.name}
+                    />
+                    ))}
+            
+            </div>
         </div>
     )
 }
