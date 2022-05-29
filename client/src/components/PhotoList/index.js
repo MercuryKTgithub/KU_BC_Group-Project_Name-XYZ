7466bcfcfa33a36754caa47a334646a8e7920583
bryce <blarkin13@gmail.com>
2022-05-29 01:27:18 -0500

    photos

---
+++ client/src/components/PhotoList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import photo from '../../assets/small/wk-1.jpg'
+
+function PhotoList() {
+    return(
+        <div>
+            <img
+            src={photo}
+            alt="wedding cake"
+            />
+
+        </div>
+    )
+}
+
+export default PhotoList;
